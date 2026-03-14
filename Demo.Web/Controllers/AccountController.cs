b1de79d5824ae561de7a04ec0113d84c462d45d7
MatilDaVDRochet <2121030019@student.humg.edu.vn>
2026-03-15 03:30:14 +0700

    fix duplicate email check

--- Demo.Web/Controllers/AccountController.cs
+++ Demo.Web/Controllers/AccountController.cs
@@ -175,12 +175,12 @@ namespace Demo.Web.Controllers
                 if (ModelState.IsValid)
                 {
                     var existedUser = _userRepository.Find(x => x.Email == model.Email).ToList();
-                    if(existedUser != null)
+                    if (existedUser.Any())
                     {
                         TempData[TempDataKey.Warning] = TempDataMessage.DuplicateEmail;
                         return View(model);
                     }
-                    
+
                     HttpContext.Session.SetObject("RegisterData", model); // đẩy data vào session và lấy lại khi xác thực mã thành công
 
                     // tạo mã xác thực và gửi đến mail đăng ký
